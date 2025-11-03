61f3f53d28e330a0ce2b38f0dfb999e4e8f72809
sillysilvy <silvyespindola2004@gmail.com>
2025-11-03 20:47:13 -0300

    frontend da página de perguntas criado

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 const express = require("express");
 const app = express();
+const path = require('path');
 
 //definindo o ejs como view engine
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
+app.set('views', path.join(__dirname, 'views'));
+app.engine('ejs', require('ejs').__express);
 
 app.get("/", (req,res) => {
     res.render("index");
+});
+
+app.get("/perguntar", (req,res) => {
+    res.render("perguntar");
+});
+
+app.post("/salvarpergunta", (req,res) => {
+    res.send("aaaaa");
 });
 
 app.listen(8080, () => {
